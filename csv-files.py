93ccc4eb5f77eb6aea05528e971792e5a9ef440c
Martin Michel Majcenovic <majcenovic.martin@gmail.com>
2020-05-02 18:26:51 +0200

    Updating code

--- csv-files.py
+++ csv-files.py
@@ -6,14 +6,14 @@ def first_solution():
     reader = csv.DictReader(people)
     for row in reader:
         print(row['Name'], "is", row['Gender'], "and", row['Age'], "years old")
-    # I think this method is better because it's easier to read
 
 
 def second_solution():
     data = people.readlines()
     for line in data[1:]:
-        print(line.split(',')[0], "is", line.split(',')[2].strip('\n'), "and", line.split(',')[1], "years old.")
-    # Without ".strip('\n')" the rest of the text gets written in new line, don't know why.
+        name, age, gender = line.split(",")
+        gender = gender.replace("\n", "")
+        print(f"{name} is {gender} and {age} years old.")
 
 
 with open("people.csv", "r") as people:
